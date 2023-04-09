a5a80af918caaf6abc90e8cbf73a36bcf7901151
Rick Wesley <rick.wes1@gmail.com>
2023-04-09 14:55:11 -0300

    criando header pagina home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <RouterLink class="navbar-brand" to="/"> CRUDSnap </RouterLink>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink class="nav-link px-4" to="/"> Home </RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link px-4" to="/#"> PaginaTal </RouterLink>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+              data-bs-toggle="dropdown" aria-expanded="false">
+              Perfil
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li>
+                <RouterLink class="dropdown-item" to="/exit"> Sair </RouterLink>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 </template>
